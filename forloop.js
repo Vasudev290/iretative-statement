edc4542b8282e710f59a753f65c8495a4c198684
vasudev290 <vasukesavulu@gmail.com>
2024-03-30 14:08:15 +0530

    first commit

---
+++ forloop.js
@@ -0,0 +1,32 @@
+// Write a program to print 1 to 100 numbers
+
+for( var i=1; i<=100; i=i+1){
+    console.log(i);
+}
+
+
+// Write a program to print 50 to 150 numbers
+
+for( var i=50; i<=150; i=i+10){
+    console.log(i);
+}
+
+
+// Write a program to print 15 to 10 numbers
+
+for(var i=15; i>=10; i=i-1){
+     console.log(i);
+}
+
+// Write a program to print 1 to 100 even numbers
+
+for( var i=1; i<=100; i=i+2){
+     console.log(i);
+}
+
+
+// Write a program to print 1 to 10 odd numbers
+
+for( var i=1; i<=10; i=i+3){
+     console.log(i);
+}
---
+++ while.js
@@ -0,0 +1,51 @@
+// Write a program to print 1 to 100 numbers
+
+var i=1;
+while(i<=100){
+    console.log(i);
+    i=i+1;
+}
+
+
+// Write a program to print 100 to 1 numbers
+
+var i=100;
+while(i>=1){
+    console.log(i);
+    i=i-1;
+}
+
+
+// Write a program to print 50 to 150 numbers
+
+var i=50;
+while(i<=150){
+  console.log(i);
+  i=i+10;
+}
+
+
+// Write a program to print 15 to 10 numbers
+
+var i=15;
+while(i>=10){
+  console.log(i);
+  i=i-1
+}
+
+
+// Write a program to print 1 to 10 even numbers
+
+var i=1;
+while(i<=10){
+    console.log(i);
+    i= i+2;
+}
+
+// Write a program to print 1 to 10 odd numbers
+
+var i=1;
+while(i<=10){
+    console.log(i);
+    i= i+3;
+}
